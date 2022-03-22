60b097a76ce05b7bc73ef85f5a1b83c12a2169c6
Kenny1911 <o-muzyka@mail.ru>
2022-03-22 15:48:12 +0400

    refactor: FunctionsTest

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -4,53 +4,12 @@ declare(strict_types=1);
 
 namespace Kenny1911\CloneWith\Test;
 
-use DateTime;
-use PHPUnit\Framework\TestCase;
 use function Kenny1911\CloneWith\clone_with;
 
-class FunctionsTest extends TestCase
+class FunctionsTest extends CloneTestCase
 {
-    public function testCloneWith()
+    protected function cloneWith($object, array $properties)
     {
-        $object = new class {
-            public $foo = 123;
-            public $bar = 456;
-        };
-
-        $clone = clone_with($object, ['bar' => 789]);
-
-        $this->assertSame(get_class($object), get_class($clone));
-        $this->assertSame(123, $clone->foo);
-        $this->assertSame(789, $clone->bar);
-    }
-
-    public function testCloneWithCloneableObject()
-    {
-        $dateTime = new DateTime();
-
-        $object = new class($dateTime) {
-            public $foo = 123;
-            public $bar = 456;
-            public $dateTime;
-
-            public function __construct(DateTime $dateTime)
-            {
-                $this->dateTime = $dateTime;
-            }
-
-            public function __clone()
-            {
-                $this->dateTime = clone $this->dateTime;
-            }
-        };
-
-        $clone = clone_with($object, ['bar' => 789]);
-        $this->assertSame(get_class($object), get_class($clone));
-        $this->assertSame(123, $clone->foo);
-        $this->assertSame(789, $clone->bar);
-
-        // Check, that DateTime is cloned with same value
-        $this->assertTrue($dateTime == $clone->dateTime);
-        $this->assertTrue($dateTime !== $clone->dateTime);
+        return clone_with($object, ['bar' => 789]);
     }
 }
